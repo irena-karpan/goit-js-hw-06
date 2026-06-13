7d909c660ccdd5f637f4a6866aacd87496c43d28
Irena Karpan <irenakarpan@gmail.com>
2026-06-13 22:07:28 +0200

    do task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,2 +1,43 @@
 'use strict'
 
+class Storage{
+    #items;
+
+    constructor(itemsArr) {
+        this.#items = itemsArr;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        if (this.#items.includes(itemToRemove)) {
+            for (let i = 0; i < this.#items.length; i++) {
+                if (this.#items[i].toLowerCase() === itemToRemove.toLowerCase()) {
+                    this.#items.splice(i, 1);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
